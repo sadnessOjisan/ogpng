e392b1156fea3a4cf3e46e043e07e031c9940fa5
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-16 14:14:00 +0900

    deb

--- src/helper/env.ts
+++ src/helper/env.ts
@@ -2,5 +2,6 @@ import { EnvType } from "../types/util/env";
 
 export default (): EnvType => {
   // vercelの環境変数VERCEL_ENVで 'development', 'production', 'test' を指定する
+  console.log(process.env);
   return process.env.NODE_ENV;
 };

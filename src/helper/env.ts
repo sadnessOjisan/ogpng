a1b88804779c738ac80b5ab5b16e32d90c49241f
sadness_ojisan <sadness.ojisan@gmail.com>
2020-06-16 13:49:05 +0900

    fix

--- src/helper/env.ts
+++ src/helper/env.ts
@@ -2,10 +2,5 @@ import { EnvType } from "../types/util/env";
 
 export default (): EnvType => {
   // vercelの環境変数VERCEL_ENVで 'development', 'production', 'test' を指定する
-  const env = process.env.DEPLOY_ENV;
-  if (env !== "development" && env !== "production" && env !== "test") {
-    console.log(env);
-    return process.env.NODE_ENV;
-  }
-  return env;
+  return process.env.NODE_ENV;
 };
